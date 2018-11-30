2bf5de18097ab498ba00f512ee9165324a85a473
Sharan Srivatsa <shsr7814@colorado.edu>
2018-11-30 13:12:39 -0700

    Changed Outputformat
    Co-authored-by: Prashil Bhimani <prashilbhimani24@gmail.com>

--- MapReduceJobs/AnalyticsArtifact/src/main/java/AnalyticsGroup/AnalyticsArtifact/GetTweetKeys.java
+++ MapReduceJobs/AnalyticsArtifact/src/main/java/AnalyticsGroup/AnalyticsArtifact/GetTweetKeys.java
@@ -11,21 +11,47 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.jobhistory.HistoryViewer.AnalyzedJob;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.json.JSONObject;
 
-public class getTweetKeys {
+import net.minidev.json.JSONArray;
 
-	public static class GetTweetKeysMapper extends Mapper<Object, Text, Text, IntWritable> {
+public class GetTweetKeys {
+	public static String getType(Object tweetvalue) {
+		String type = "Is Nullable";
+		if(tweetvalue instanceof Long) {
+			type = "Long";
+		} else if(tweetvalue instanceof Double) {
+			type = "Double";
+		} else if(tweetvalue instanceof Float) {
+			type = "Float";
+		} else if(tweetvalue instanceof Integer) {
+			type = "Integer";
+		} else if(tweetvalue instanceof Boolean) {
+			type = "Boolean";
+		} else if(tweetvalue instanceof String) {
+			type = "String";
+		} else if(tweetvalue instanceof Byte) {
+			type = "Byte";
+		} else if(tweetvalue instanceof JSONArray) {
+			type = "JSONArray";
+		} else if(tweetvalue instanceof JSONObject) {
+			type = "JSONObject";
+		} 
+		return type;
+	}
+	
+	public static class GetTweetKeysMapper extends Mapper<Object, Text, Text, Text> {
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			//System.out.println("mapper input is: "+ value.toString());			
 			try {
 				JSONObject tweetJson = new JSONObject(value.toString());
 				for(String tweetfield : JSONObject.getNames(tweetJson)) {
 					//System.out.println("field is: " + tweetfield);
-					context.write(new Text(tweetfield), new IntWritable(1));
+					context.write(new Text(tweetfield), value);
 				}							
 			} catch(Exception e) {
 				e.printStackTrace();
@@ -34,16 +60,24 @@ public class getTweetKeys {
 
 	}
 
-	public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {		
-		public void reduce(Text term, Iterable<IntWritable> ones, Context context) throws IOException, InterruptedException {			
+	public static class SumReducer extends Reducer<Text, Text, Text, Text> {		
+		public void reduce(Text term, Iterable<Text> ones, Context context) throws IOException, InterruptedException {			
 			int count = 0;
-			Iterator<IntWritable> iterator = ones.iterator();			
+			String type = "unknown";
+			
+			Iterator<Text> iterator = ones.iterator();			
 			while(iterator.hasNext()) {
-				IntWritable tweetKey = iterator.next();
-//				System.out.println("tweetKey is : " + tweetKey);
+				Text tweet = iterator.next();
+				JSONObject tweetJson = new JSONObject(tweet.toString());
+				Object value = tweetJson.get(term.toString());
+				type = GetTweetKeys.getType(value);
 				count++;						
-			}							
-			context.write(term, new IntWritable(count));
+			}
+			JSONObject result = new JSONObject();
+			result.put("tweetKey", term.toString());
+			result.put("keyCount", count);
+			result.put("dataType", type);
+			context.write(new Text(result.toString()), new Text(""));
 
 		}
 	}
@@ -62,12 +96,12 @@ public class getTweetKeys {
 		f.setNumReduceTasks(1);
 		System.out.println("Setting Job");		
 		Job job = Job.getInstance(conf, "Job name: WordCount");
-		job.setJarByClass(getTweetKeys.class);
+		job.setJarByClass(GetTweetKeys.class);
 		job.setMapperClass(GetTweetKeysMapper.class);
 		job.setReducerClass(SumReducer.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 		job.setNumReduceTasks(1);
 
 		System.out.println("Setting files");
