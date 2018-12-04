5bdf537b34d900e7234cca2001a87422c47ac33f
Sharan Srivatsa <shsr7814@colorado.edu>
2018-12-03 17:05:37 -0700

    get API in place

--- MapReduceJobs/DCSElasticSearchArtifact/src/main/java/DCSElasticSearchGroup/DCSElasticSearchArtifact/ESClient.java
+++ MapReduceJobs/DCSElasticSearchArtifact/src/main/java/DCSElasticSearchGroup/DCSElasticSearchArtifact/ESClient.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -18,8 +21,8 @@ class ESAPI {
 		this.client = new RestHighLevelClient(RestClient.builder(new HttpHost(host, port, "http"),new HttpHost("host", port+1, "http")));
 	}
 
-	public IndexResponse getIndexResponse(Map<String, Object> jsonMap, String index, String doc, String id) {
-		IndexRequest indexRequest = new IndexRequest(index, doc, id).source(jsonMap);			 
+	public IndexResponse getIndexResponse(Map<String, Object> jsonMap, String index, String type, String id) {
+		IndexRequest indexRequest = new IndexRequest(index, type, id).source(jsonMap);			 
 		try {
 			IndexResponse indexResponse = this.client.index(indexRequest, RequestOptions.DEFAULT);
 			return indexResponse;
@@ -28,36 +31,62 @@ class ESAPI {
 		}
 		return null;			
 	}
+
+	public GetResponse getResponse(String index, String type, String id ) {
+		GetRequest getRequest = new GetRequest(index, type,  id);
+		try {
+			GetResponse getResponse = this.client.get(getRequest, RequestOptions.DEFAULT);
+			return getResponse;
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
 
 public class ESClient 
 {
 	public static Map<String, Object> createdummyInput() {
-    	Map<String, Object> jsonMap = new HashMap<String, Object>();
-    	jsonMap.put("user", "sharan");
-    	jsonMap.put("postDate", new Date());
-    	jsonMap.put("message", "trying out Elasticsearch");
-    	return jsonMap;
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		jsonMap.put("user", "sharan");
+		jsonMap.put("postDate", new Date());
+		jsonMap.put("message", "trying out Elasticsearch");
+		return jsonMap;
 	}
-    public static void main( String[] args ) throws IOException
-    {
-    	ESAPI esclient = new ESAPI("localhost", 9200);
+	public static void main( String[] args ) throws IOException
+	{
+		ESAPI esclient = new ESAPI("localhost", 9200);
 
-    	Map<String, Object> jsonMap = createdummyInput();
-    	
-    	String index = "apiposts";
-    	String doc = "doc";
-    	String id = "1";
-    	// Index API
-    	IndexResponse indexResponse = esclient.getIndexResponse(jsonMap, index, doc, id);
-    	if(indexResponse != null) {
-    		System.out.println(String.format("index: %s, type: %s, id: %s	", indexResponse.getIndex(), indexResponse.getType(), indexResponse.getId()));	
-    	}
-    	    	
-    	System.out.println("Done");
-    	
+		Map<String, Object> jsonMap = createdummyInput();
 
-    	
-    	
-    }
+		String index = "apiposts";
+		String type = "doc";
+		String id = "1";
+
+		// Index API
+		IndexResponse indexResponse = esclient.getIndexResponse(jsonMap, index, type, id);
+		if(indexResponse != null) {
+			System.out.println(String.format("index: %s, type: %s, id: %s	", indexResponse.getIndex(), indexResponse.getType(), indexResponse.getId()));	
+		}
+		
+		// Get API
+		GetResponse getResponse = esclient.getResponse(index, type, id);
+		if(getResponse != null) {
+			System.out.println(String.format("index: %s, type: %s, id: %s	", getResponse.getIndex(), getResponse.getType(), getResponse.getId()));
+			if (getResponse.isExists()) {
+			    long version = getResponse.getVersion();
+			    String sourceAsString = getResponse.getSourceAsString();        
+			    Map<String, Object> sourceAsMap = getResponse.getSourceAsMap(); 
+			    byte[] sourceAsBytes = getResponse.getSourceAsBytes();
+			    System.out.println(String.format("version is %d\nsource is %s", version, sourceAsString));
+			}
+		}
+
+
+		System.out.println("Done");
+
+
+
+
+	}
 }
